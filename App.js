e40727bb1de6f186c534e54cb0866f6da1ef23e1
Betoitiro <itirotrabalhos@gmail.com>
2023-11-12 22:46:19 -0300

    organizando

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Rotas
+// Rotas 
 app.get('/', function (req, res) {
     res.render('Form');
 });
